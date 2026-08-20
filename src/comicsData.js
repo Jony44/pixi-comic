7d03a08571a5ba25d150d515952ad0410f24c939
Jony44 <maneesharavihara0@gmail.com>
2026-08-20 18:28:39 +0530

    updates

--- src/comicsData.js
+++ src/comicsData.js
@@ -1,35 +1,34 @@
-export const [comicsList, setComicsList] = useState([
-    {
-      id: 1,
-      title: 'Spider-Man (2008) Brand New Day Vol 01',
-      price: '$4.99',
-      image: 'https://i.imgur.com/SaYjAZk.jpeg', // මෙතැනට අලුත් නම දාන්න
-      checkoutUrl: 'https://pixicomics.lemonsqueezy.com/checkout/buy/3e514cbf-36e7-4368-b3ab-7b2d75772885?discount=0',
-      totalPages: 197
-    },
-    {
-      id: 2,
-      title: 'spider-man (2009) Brand New Day Vol 02',
-      price: '$4.99',
-      image: 'https://i.imgur.com/nMMCOjV.jpeg', // මෙතැනටත්
-      checkoutUrl: 'https://pixicomics.lemonsqueezy.com/checkout/buy/390e469e-ca84-44fc-8263-b518d8c25947?discount=0',
-      totalPages: 168
-    },
-     {
-      id: 3,
-      title: 'spider-man (2009) Brand New Day Vol 03',
-      price: '$4.99',
-      image: 'https://i.imgur.com/vdFhVg3.jpeg', // මෙතැනටත්
-      checkoutUrl: 'https://pixicomics.lemonsqueezy.com/checkout/buy/2c35d615-e473-4f82-8edd-30433118b8e7?discount=0',
-      totalPages: 128
-     },
-      {
-      id: 4,
-      title: 'Absolute Green Arrow #4 (2026)',
-      price: '$4.99',
-      image: 'https://i.imgur.com/L6qaQCy.jpeg', // මෙතැනටත්
-      checkoutUrl: 'https://pixicomics.lemonsqueezy.com/checkout/buy/5f9d79d9-ba6d-4c33-b155-7f479790e27a',
-      totalPages: 28
-     }
-
-  ]);
+export const comicsList = [
+  {
+    id: 1,
+    title: 'Spider-Man (2008) Brand New Day Vol 01',
+    price: '$4.99',
+    image: 'https://i.imgur.com/SaYjAZk.jpeg',
+    checkoutUrl: 'https://pixicomics.lemonsqueezy.com/checkout/buy/3e514cbf-36e7-4368-b3ab-7b2d75772885?discount=0',
+    totalPages: 197
+  },
+  {
+    id: 2,
+    title: 'spider-man (2009) Brand New Day Vol 02',
+    price: '$4.99',
+    image: 'https://i.imgur.com/nMMCOjV.jpeg',
+    checkoutUrl: 'https://pixicomics.lemonsqueezy.com/checkout/buy/390e469e-ca84-44fc-8263-b518d8c25947?discount=0',
+    totalPages: 168
+  },
+  {
+    id: 3,
+    title: 'spider-man (2009) Brand New Day Vol 03',
+    price: '$4.99',
+    image: 'https://i.imgur.com/vdFhVg3.jpeg',
+    checkoutUrl: 'https://pixicomics.lemonsqueezy.com/checkout/buy/2c35d615-e473-4f82-8edd-30433118b8e7?discount=0',
+    totalPages: 128
+  },
+  {
+    id: 4,
+    title: 'Absolute Green Arrow #4 (2026)',
+    price: '$4.99',
+    image: 'https://i.imgur.com/L6qaQCy.jpeg',
+    checkoutUrl: 'https://pixicomics.lemonsqueezy.com/checkout/buy/5f9d79d9-ba6d-4c33-b155-7f479790e27a',
+    totalPages: 28
+  }
+];
